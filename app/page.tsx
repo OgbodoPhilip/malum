e1e738869055352d9f7b604b6248f19634622184
ogbodo philip <mrogbodophilip@gmail.com>
2025-11-15 06:46:57 -0800

    first display

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import ProductViewer from "@/components/ProductViewer";
 
 
 
 
 export default function Home() {
   return (
-    <main>
+    <main className="md:mt-20">
       <Navbar />
       <Hero/>
+      <ProductViewer/>
     </main>
   )
 }
