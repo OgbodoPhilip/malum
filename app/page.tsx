17d80141a2e21222fa4301d99f4810c2f16457dc
ogbodo philip <mrogbodophilip@gmail.com>
2025-11-16 02:58:25 -0800

    video updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-import ProductViewer from "@/components/ProductViewer";
+
 import { ScrollTrigger,SplitText } from "gsap/all";
 import gsap from "gsap";
 import ShowCase from "@/components/ShowCase";
@@ -12,7 +12,7 @@ export default function Home() {
     <main className="md:mt-20">
       <Navbar />
       <Hero/>
-      <ProductViewer/>
+   
       <ShowCase/>
     </main>
   )
